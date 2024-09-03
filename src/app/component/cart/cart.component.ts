a7437457c72a79c3a8f01de7edce9947e5144192
Amol799 <166220003+Amol799@users.noreply.github.com>
2024-09-03 20:51:51 +0530

    cart feature

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -7,17 +7,25 @@ import { CartService } from '../../services/cart.service';
   styleUrl: './cart.component.css'
 })
 export class CartComponent implements OnInit {
-
+  public productList : any;
   totalCartItem : number =0;
+  grandTotal : number =0;
   constructor(private cartsvc: CartService){}
 
   ngOnInit(): void {
     this.cartsvc.getProducts().subscribe(
       res =>{
+        this.grandTotal = 0;
+         this.productList = res;
          this.totalCartItem = res.length;
+
+         this.productList.map((a:any) => {
+          this.grandTotal += a.price;
+        })
+
       }
     )
-    throw new Error('Method not implemented.');
+    console.log(this.productList);
   }
 
 }
--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -14,6 +14,7 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
     this.cartsvc.getProducts().subscribe(
       res =>{
+
          this.tatalCartItem = res.length;
       }
     )
