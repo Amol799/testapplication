2dfd0a45b485e71c155ce65740882a7120ce5fc6
Amol799 <166220003+Amol799@users.noreply.github.com>
2024-09-05 00:18:27 +0530

    ui chnages

--- src/app/component/user/login/login.component.ts
+++ src/app/component/user/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+
+export class LoginComponent implements OnInit {
+
+
+  username : string = "";
+  password : string ="";
+  mobile : string ="";
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
+  login(){
+    console.log("userid : " + this.username  + "password :" + this.password)
+  }
+
+  register(){
+
+    console.log("userid : " + this.username  + "password :" + this.password + "mobile" + this.mobile);
+
+  }
 
 }
